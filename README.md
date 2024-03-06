08ecda68e79a93642f0b1f9486905d9a007f702b
mlechi02 <mlechi001@gmail.com>
2024-03-06 17:20:58 +0300

    this is my user input code

---
+++ README.md
@@ -0,0 +1,1 @@
+this code request input from users and print the output
---
+++ user_input.py
@@ -0,0 +1,4 @@
+name= input("enter your name: ")
+age= input("enter your age: ")
+location= input("enter your location: ")
+print(f"your name is {name}. your age is {age}.you live in {location}.")
